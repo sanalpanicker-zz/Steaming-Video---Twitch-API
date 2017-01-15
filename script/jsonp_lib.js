2a7370041cc9276662da4a935ab75b894ff53beb
Sanal <sanalpanicker1987@gmail.com>
2017-01-15 01:48:14 -0800

    PS Streaming demo

---
+++ script/jsonp_lib.js
@@ -0,0 +1,26 @@
+var _jsonp = _jsonp || {};
+(function() {
+    _jsonp = {
+        call: function(base_url, client_id, search_query, call_backfn) {
+            this[call_backfn] = function(data) {
+                console.log(data);
+                display(data);
+                this.clean();
+                //delete this[call_backfn];
+            }
+            var url = base_url + "?q=" + search_query;
+            url = url + "&client_id=" + client_id;
+            url = url + "&limit=" + 100;
+            url = url + "&callback=" + "_jsonp." + call_backfn;
+            var scriptElement = document.createElement("script");
+            scriptElement.id = "jsonp";
+            scriptElement.src = url;
+            document.head.appendChild(scriptElement);
+
+        },
+        clean: function() {
+            var scriptElement = document.getElementById("jsonp");
+            scriptElement.parentNode.removeChild(scriptElement);
+        }
+    };
+})();
---
+++ script/pstv.js
@@ -0,0 +1,45 @@
+var BASE_URL = "https://api.twitch.tv/kraken/search/channels",
+    CLIENT_ID = "3ayqtffruo2goxf0cvyp75wjm28g4pq",
+    search_query = "starcraft";
+//default call
+search(search_query);
+var l = document.getElementById('loader');
+l.style.visibility = 'visible';
+var c = document.getElementById('container');
+c.style.visibility = 'hidden';
+
+function search(e) {
+    if (e.value) {
+        search_query = e.value;
+    } else {
+        search_query = "starcraft";
+    }
+
+    _jsonp.call(BASE_URL, CLIENT_ID, search_query, "pstv_CallBack");
+}
+
+
+function display(result) {
+    if (result) {
+        l.style.visibility = 'hidden';
+        c.style.visibility = 'visible';
+    }
+    var rarr = result.channels,
+        dlist = "",
+        dtotal = "";
+    for (var i = 0; i < rarr.length; i++) {
+
+        dlist += "<div class='tile'><div class='logo'><a href='" + rarr[i].url + "'><img src='" + rarr[i].logo + "'></img></a></div>" +
+            "<div class='info'>" +
+            "<span class='display_name'>" + rarr[i].display_name + "</span><br>" +
+            "<span class='game'>" + rarr[i].game + "</span>-<span class='views'>" + rarr[i].views + "</span><br>" +
+            "<span class='status'>" + rarr[i].status + "</span>" +
+            "</div></div>";
+    }
+
+    dtotal = "<label class='rtotal_label'>Total results</label><br><span class='rtotal_val'><h1>" + rarr.length + "</h1></span>";
+    var d = document.getElementById('data_block');
+    var t = document.getElementById('total_result');
+    d.innerHTML = dlist;
+    t.innerHTML = dtotal;
+}
